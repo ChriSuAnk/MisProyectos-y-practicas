c74a9e1b0f7aeaf64e604b1dcf8646ad694af57e
SuAnK <104573632+ChriSuAnk@users.noreply.github.com>
2023-10-17 01:43:39 -0500

    agregado a una carpeta personal

---
+++ Projects_Udemy/IntroduccionJS/js/scripts.js
@@ -0,0 +1,14 @@
+const num1 = 2
+const num2 = 3
+
+const sumaResult = (num1+num2)*num1
+
+const cliente = {
+    nombre : "christian",
+    apellidos : "mendoza amado",
+    edad : 21,
+    signo : "leo"
+};
+cliente.nombre = "Henry";
+
+console.log(cliente.nombre+ " es del signo sodiacal "+ cliente.signo)
